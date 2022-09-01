7bc63947c4b03553ce0460f40242ea867b78cee7
itsmanojreddyy <94695026+itsmanojreddyy@users.noreply.github.com>
2022-09-01 12:28:51 +0530

    Initial setup for pytest for paytm

---
+++ tests/PaytmSignIn.py
@@ -0,0 +1,22 @@
+import time
+
+import allure
+import pytest
+
+from tests.test_base import BaseTest
+
+
+@allure.epic("Paytm Sign In")
+@allure.severity(allure.severity_level.CRITICAL)
+@pytest.mark.security
+@pytest.mark.usefixtures("setup")
+class TestPaytmSignIn(BaseTest):
+
+    # def test_click_signin(self):
+    #     self.pages['PaytmSignIn_Page'].click_signin()
+
+    @allure.description("Click on Paytm Sign in button")
+    @allure.title("Click on Paytm Sign in")
+    def test_search_product(self):
+        self.pages['PaytmSignIn_Page'].click_signin()
+        time.sleep(2)
