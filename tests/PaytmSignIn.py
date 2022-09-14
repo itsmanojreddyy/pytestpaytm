e0a98d157f311e8bd140520cb9a52869ae0ef42f
itsmanojreddyy <94695026+itsmanojreddyy@users.noreply.github.com>
2022-09-14 13:21:05 +0530

    CHANGES AS PER LOCAL DIRECTORY

--- tests/PaytmSignIn.py
+++ tests/PaytmSignIn.py
@@ -15,11 +15,10 @@ class TestPaytmSignIn(BaseTest):
     # def test_click_signin(self):
     #     self.pages['PaytmSignIn_Page'].click_signin()
 
-    # @allure.description("Click on Paytm Sign in button")
-    # @allure.title("Click on Paytm Sign in")
-    # def test_click_SignIn(self):
-    #     pass
-    # self.pages['PaytmSignIn_Page'].click_signin()
+    @allure.description("Click on Paytm Sign in button")
+    @allure.title("Click on Paytm Sign in")
+    def test_click_SignIn(self):
+        self.pages['PaytmSignIn_Page'].click_signin()
 
     @allure.description("Click on Paytm Sign in button")
     @allure.title("Click on Paytm Sign in")
